35b54ee846abde6f773b20d1d32b7dd8d7369dc1
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-15 20:45:43 +0100

    add base to Vite config based on Node environment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config
 export default defineConfig({
+    base: process.env.NODE_ENV === 'development' ? '/' : '/vue-quiz/',
 	plugins: [vue()],
 	resolve: {
 		alias: {'@': fileURLToPath(new URL('./src', import.meta.url))}
